68a46b9b9937cda3ee9e7208462f8c4584b1dd17
rc2pc2 <petriccarcc@gmail.com>
2024-11-07 19:53:12 +0100

    added testing comment

--- src/App.java
+++ src/App.java
@@ -8,3 +8,4 @@ public class App {
 // % => il compilatore compila Car.java in un file scritto in bytecode Car.class
 // # buonasera JVM, che ne direste di eseguire il mio file Car.class?
     // @ la traduco dal bytecode ad un linguaggio macchina comprensibile dalla macchina che la esegue, e poi la eseguo
+    // Aggiungo un commento qualsiasi
