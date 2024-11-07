d8727bcfc95563f1677800e0ed5a2bfc29cb3dce
rc2pc2 <petriccarcc@gmail.com>
2024-11-07 19:52:13 +0100

    Project initialization

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
+// ! Posto che non ci siano errori nel nostro file Car.java
+// % => il compilatore compila Car.java in un file scritto in bytecode Car.class
+// # buonasera JVM, che ne direste di eseguire il mio file Car.class?
+    // @ la traduco dal bytecode ad un linguaggio macchina comprensibile dalla macchina che la esegue, e poi la eseguo
